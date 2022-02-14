1b707ce8074fbb1adbc2ea4e1d9f0dd75da9bec5
cwoodrow <chris.woodrow@maif.fr>
2022-02-14 12:07:58 +0100

    Add cloud connnexion instructions for mongo

--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -4,6 +4,10 @@ var fs = require("fs");
 
 var MongoClient = mongodb.MongoClient;
 var mongoUrl = "mongodb://localhost:27017";
+// /!\ si vous utilisez une version cloud, la chaîne de connexion devrait ressembler à ça
+// var mongoUrl = "mongodb+srv://mongo:<password>@<url>/<database>>?retryWrites=true&w=majority";
+// vous trouvez cette chaine dans l'interface Atlas Connext > Conect to your application
+
 const dbName = "marvel";
 const collectionName = "heroes";
 
