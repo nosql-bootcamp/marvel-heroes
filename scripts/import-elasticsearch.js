0a59007375c200a78de0284440d3edebc7e49449
cwoodrow <chris.woodrow@maif.fr>
2022-02-14 12:03:20 +0100

    Fix Typo

--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const { Client } = require('@elastic/elasticsearch')
 const esClient = new Client({ node: 'http://localhost:9200' })
-// /!\ si vous utiliser une version cloud, la chaîne de connexion devrait ressembler à ça
+// /!\ si vous utilisez une version cloud, la chaîne de connexion devrait ressembler à ça
 // const esClient = new Client({
 //   node: 'https://<endpoint>:<port>',
 //   auth: {
