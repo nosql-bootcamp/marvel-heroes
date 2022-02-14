dbd4df0c63b37caa6ddd1edef0a471a2ae7627a6
cwoodrow <chris.woodrow@maif.fr>
2022-02-14 12:01:58 +0100

    Add cloud connexion instruction for ES

--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 
 const { Client } = require('@elastic/elasticsearch')
 const esClient = new Client({ node: 'http://localhost:9200' })
+// /!\ si vous utiliser une version cloud, la chaîne de connexion devrait ressembler à ça
+// const esClient = new Client({
+//   node: 'https://<endpoint>:<port>',
+//   auth: {
+//     username: 'elastic',
+//     password: '<password>'
+//   }
+// });
+
 const heroesIndexName = 'heroes'
 
 // TODO
