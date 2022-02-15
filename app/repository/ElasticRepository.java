83e57bab5721449638f627394a9e38785e7c0c24
Mathieu.MERILLON <mathieu.merillon@paysdelaloire.fr>
2022-02-15 18:47:17 +0100

    Repositories

--- app/repository/ElasticRepository.java
+++ app/repository/ElasticRepository.java
@@ -1,15 +1,19 @@
 package repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import env.ElasticConfiguration;
 import env.MarvelHeroesConfiguration;
 import models.PaginatedResults;
 import models.SearchedHero;
+import play.libs.Json;
 import play.libs.ws.WSClient;
 import utils.SearchedHeroSamples;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
@@ -28,22 +32,54 @@ public class ElasticRepository {
 
 
     public CompletionStage<PaginatedResults<SearchedHero>> searchHeroes(String input, int size, int page) {
-        return CompletableFuture.completedFuture(new PaginatedResults<>(3, 1, 1, Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan())));
-        // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+        int from = size * (page-1);
+        String jsonRequest = "{\"from\": " + from + ",\"size\": " + size + ",\"query\": {\"wildcard\": {\"name\": {\"value\": \"*" + input + "*\"}}}}";
+        return wsClient.url(elasticConfiguration.uri + "/heroes/_search")
+             .post(Json.parse(jsonRequest))
+             .thenApply(response -> {
+                 JsonNode searchResult = Json.parse(response.getBody()).get("hits");
+                 int total = searchResult.get("total").get("value").asInt();
+
+                 List<SearchedHero> hits = new ArrayList<>(total);
+                 for (Iterator<JsonNode> it = searchResult.get("hits").elements(); it.hasNext(); ) {
+                    JsonNode hit = it.next().get("_source");
+                    hits.add(
+                            new SearchedHero(
+                                    hit.get("id").asText(),
+                                    hit.get("imageUrl").asText(),
+                                    hit.get("name").asText(),
+                                    hit.get("universe").asText(),
+                                    hit.get("gender").asText()
+                            )
+                    );
+                 }
+                 return new PaginatedResults<>(total, page, total/size + 1, hits);
+             });
     }
 
     public CompletionStage<List<SearchedHero>> suggest(String input) {
-        return CompletableFuture.completedFuture(Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan()));
-        // TODO
-        // return wsClient.url(elasticConfiguration.uri + "...")
-        //         .post(Json.parse("{ ... }"))
-        //         .thenApply(response -> {
-        //             return ...
-        //         });
+         return CompletableFuture.completedFuture(Arrays.asList(SearchedHeroSamples.IronMan(), SearchedHeroSamples.MsMarvel(), SearchedHeroSamples.SpiderMan()));
+        //String jsonRequest = "{\"from\": " + from + ",\"size\": " + size + ",\"query\": {\"wildcard\": {\"name\": {\"value\": \"*" + input + "*\"}}}}";
+        /*return wsClient.url(elasticConfiguration.uri + "/heroes/_suggest")
+                .post(Json.parse(jsonRequest))
+                .thenApply(response -> {
+                    JsonNode searchResult = Json.parse(response.getBody()).get("hits");
+                    int total = searchResult.get("total").get("value").asInt();
+
+                    List<SearchedHero> hits = new ArrayList<>(total);
+                    for (Iterator<JsonNode> it = searchResult.get("hits").elements(); it.hasNext(); ) {
+                        JsonNode hit = it.next().get("_source");
+                        hits.add(
+                                new SearchedHero(
+                                        hit.get("id").asText(),
+                                        hit.get("imageUrl").asText(),
+                                        hit.get("name").asText(),
+                                        hit.get("universe").asText(),
+                                        hit.get("gender").asText()
+                                )
+                        );
+                    }
+                    return new PaginatedResults<>(total, page, total/size, hits);
+                });*/
     }
 }
