ac96defeac8ca94e3c7358a57407218045b2c446
Shauni <shaunisoul@protonmail.com>
2022-02-15 15:50:40 +0100

    Import to elastic

--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -2,17 +2,73 @@ const csv = require('csv-parser');
 const fs = require('fs');
 
 const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({ node: 'http://localhost:9200' })
-// /!\ si vous utilisez une version cloud, la chaîne de connexion devrait ressembler à ça
-// const esClient = new Client({
-//   node: 'https://<endpoint>:<port>',
-//   auth: {
-//     username: 'elastic',
-//     password: '<password>'
-//   }
-// });
+const { Readable } = require('stream');
 
-const heroesIndexName = 'heroes'
+const ELASTIC_SEARCH_URI = 'http://localhost:9200';
+const HEROES_INDEX_NAME = 'heroes'
 
-// TODO
-console.log("TODO ;-)");
+function getArrayOfString(data = '') {
+    if (data == '') {
+        return [];
+    }
+    return data.split(',').map(i => i.trim());
+}
+
+function formatData(data) {
+    return {
+        ...data,
+        identity: {
+            ...data.identity,
+            secretIdentities: getArrayOfString(data.identity?.secretIdentities),
+            aliases: getArrayOfString(data.identity?.aliases)
+        },
+        teams: getArrayOfString(data?.teams),
+        powers: getArrayOfString(data?.powers),
+        partners: getArrayOfString(data?.partners),
+        creators: getArrayOfString(data?.creators)
+    };
+}
+
+async function run() {
+    const client = new Client({ node: ELASTIC_SEARCH_URI});
+
+    // Drop index if exists
+    await client.indices.delete({
+        index: HEROES_INDEX_NAME,
+        ignore_unavailable: true
+    });
+
+    await client.indices.create({
+        index: HEROES_INDEX_NAME,
+        body : {
+            // TODO configurer l'index https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
+            // mappings: {
+            //     properties: {
+            //         "location": { "type": "geo_point" }
+            //     }
+            // }
+        }
+    });
+
+
+    let heroes = [];
+    fs.createReadStream('./all-heroes.csv')
+        .pipe(csv())
+        .on('data', data => heroes.push(formatData(data)))
+        .on('end', async () => {
+            const result = await client.helpers.bulk({
+                datasource: Readable.from(heroes),
+                onDocument (doc) {
+                    return {
+                        index: { _index: HEROES_INDEX_NAME },
+                        create: { doc }
+                    }
+                }
+            });
+
+            console.log(result);
+            await client.close();
+        });
+}
+
+run().catch(console.log);
