447d7c3b05bc696e868c7bc06c49e95e08d50d64
Mathieu.MERILLON <mathieu.merillon@paysdelaloire.fr>
2022-02-15 18:42:45 +0100

    Exercice Marvel-Heroes

--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -1,18 +1,40 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { Client } = require('@elastic/elasticsearch')
-const esClient = new Client({ node: 'http://localhost:9200' })
-// /!\ si vous utilisez une version cloud, la chaîne de connexion devrait ressembler à ça
-// const esClient = new Client({
-//   node: 'https://<endpoint>:<port>',
-//   auth: {
-//     username: 'elastic',
-//     password: '<password>'
-//   }
-// });
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({
+    node: `http://${process.env.ELASTICSEARCH_HOST}:${process.env.ELASTICSEARCH_PORT}`
+});
 
 const heroesIndexName = 'heroes'
+client.indices.create({ index: heroesIndexName }, (err, resp) => {
+    if (err) console.trace(err.message);
+});
 
-// TODO
-console.log("TODO ;-)");
+let heroes = [];
+fs
+    .createReadStream('./all-heroes.csv')
+    .pipe(csv())
+    .on('data', data => {
+        heroes.push(data);
+    })
+    .on('end', () => {
+        client.bulk(createBulkInsertQuery(heroes), (err, resp) => {
+            if (err) console.trace(err.message);
+            else console.log(`Inserted ${resp.body.items.length} heroes`);
+            client.close();
+        });
+    });
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(actors) {
+    const body = actors.reduce((acc, hero) => {
+        acc.push({ index: { _index: heroesIndexName, _type: '_doc', _id: hero.id } })
+        acc.push(hero)
+        return acc
+    }, []);
+
+    return { body };
+}
